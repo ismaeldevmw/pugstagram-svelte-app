5e2b5b0477ca61f037fea200e038e8821020df56
Ismael López <lc.ismael@hotmail.com>
2020-06-12 01:10:49 -0500

    :recycle: files indentation refactor
    '

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,14 +1,14 @@
 <script>
-let year = new Date().getFullYear();
+  let year = new Date().getFullYear();
 </script>
 
 <style>
-	.Footer {
-		font-size: 11px;
-		letter-spacing: 1px;
-		font-weight: normal;
-		color: #c7c7c7;
-	}
+  .Footer {
+    font-size: 11px;
+    letter-spacing: 1px;
+    font-weight: normal;
+    color: #c7c7c7;
+  }
 </style>
 
 <div class="Footer">
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,21 +1,21 @@
 <script>
-	import { onMount } from 'svelte';
-	import Header from '../components/Header.svelte';
-	import Main from '../components/Main.svelte';
-	import TimeLine from '../components/TimeLine.svelte';
-	import Sidebar from '../components/Sidebar.svelte';
+  import { onMount } from "svelte";
+  import Header from "../components/Header.svelte";
+  import Main from "../components/Main.svelte";
+  import Sidebar from "../components/Sidebar.svelte";
+  import TimeLine from "../components/TimeLine.svelte";
 
-	const API = 'https://us-central1-pugstagram-co.cloudfunctions.net/data';
-	let data = {};
+  let data = {};
+  const API = "https://us-central1-pugstagram-co.cloudfunctions.net/data";
 
-	onMount(async () => {
-		const response = await fetch(API);
-		data = await response.json();
-	});
+  onMount(async () => {
+    const response = await fetch(API);
+    data = await response.json();
+  });
 </script>
 
 <Header />
-<Main> 
-	<TimeLine posts={data.posts} />
-	<Sidebar {...data.user} />
+<Main>
+  <TimeLine posts={data.posts} />
+  <Sidebar {...data.user} />
 </Main>
